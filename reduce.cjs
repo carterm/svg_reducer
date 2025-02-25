5f6c8797eaceca0f3c6caf39e86685ccc87e7c09
Carter Medlin <carterm@users.noreply.github.com>
2025-02-25 12:22:13 -0800

    better

--- reduce.cjs
+++ reduce.cjs
@@ -82,7 +82,7 @@ fs.mkdir(outputDir, { recursive: true }, mkdirErr => {
         Math.round((parseFloat(match) * 10 * scale) / 10).toString()
       ); // Round decimals to 1 decimal place
 
-      d = d.replace(/s0 0 0 0(?![\d.])/gim, "");
+      d = d.replace(/s0 0 0 0(?![\d.])/gim, ""); // Remove "s" followed by 0 0 0 0, but not if followed by a digit or a decimal
       d = d.replace(/m[^clshv]*(m)/gim, "$1"); // Remove consecutive "M" commands
 
       d = d.replace(/c\d+ 0 \d+ 0 (\d+) 0/gm, "h$1"); // horizontal line
@@ -90,7 +90,7 @@ fs.mkdir(outputDir, { recursive: true }, mkdirErr => {
       d = d.replace(/c(?:0|-\d+) 0-\d+ 0-(\d+) 0/gm, "h-$1"); //negative horizontal line
       d = d.replace(/c0(?:-\d+|\s0) 0-\d+ 0-(\d+)/gm, "v-$1"); //negative vertical line
 
-      d = d.replace(/c0 0 0 0 (-?\d+) (-?\d+)/gim, "l$1 $2"); // line
+      d = d.replace(/c0 0 0 0 (\d+) (\d+)/gim, "l$1 $2"); // line
 
       d = d.replace(/l0 (\d+)/gim, "v$1"); // line to vertical
       d = d.replace(/l(\d+) 0/gim, "h$1"); // line to horizontal
