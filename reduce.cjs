94de683db9db081b55ed8cacb379d16c84d3f458
Carter Medlin <carterm@users.noreply.github.com>
2025-02-25 16:06:31 -0800

    v lines

--- reduce.cjs
+++ reduce.cjs
@@ -94,15 +94,24 @@ fs.mkdir(outputDir, { recursive: true }, mkdirErr => {
       d = d.replace(/l0\s*(-?\d+)/gm, "v$1"); // line to vertical
       d = d.replace(/l(-?\d+) 0/gm, "h$1"); // line to horizontal
 
-      d = d.replace(/(v|h)0(?![\d.])/gm, ""); // Remove "v" or "h" followed by the number 0, but not if followed by a digit or a decimal
-
-      d = d.replace(/h-?\d+(?:\s*h-?\d+)+/gm, match => {
+      /**
+       * Sums the values of consecutive horizontal or vertical lines.
+       * @param {string} hv - The command character ('h' for horizontal, 'v' for vertical).
+       * @param {string} match - The matched string containing the commands and values.
+       * @returns {string} - The summed command string.
+       */
+      const sumHV = (hv, match) => {
         const sum = match
-          .split("h")
+          .split(hv)
           .filter(Boolean)
           .reduce((acc, num) => acc + parseFloat(num), 0);
-        return `h${sum}`;
-      });
+        return `${hv}${sum}`;
+      };
+
+      d = d.replace(/h-?\d+(?:\s*h-?\d+)+/gm, match => sumHV("h", match)); // Sum horizontal lines
+      d = d.replace(/v-?\d+(?:\s*v-?\d+)+/gm, match => sumHV("v", match)); // Sum vertical lines
+
+      d = d.replace(/(v|h)0(?![\d.])/gm, ""); // Remove "v" or "h" followed by the number 0, but not if followed by a digit or a decimal
 
       d = d.replace(/\s+-/gm, "-"); // Remove whitespace before negative numbers
 
