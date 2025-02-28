49f2ec105f54477e575b71f761ded66dea072c63
Carter Medlin <carterm@users.noreply.github.com>
2025-02-27 17:01:36 -0800

    sloped lines

--- reduce.cjs
+++ reduce.cjs
@@ -292,22 +292,14 @@ const processData = (/** @type {string} */ data) => {
       /c\s*(-?\d+)\s*(-?\d+)\s*(-?\d+)\s*(-?\d+)\s*(-?\d+)\s*(-?\d+)/gm,
       (match, ...params) => {
         const [x0, y0, x1, y1, x2, y2] = params.map(parseFloat);
-        if (x0 === 0 && y0 === 0 && x1 === x2 && y1 === y2) {
-          return `l${x1} ${y1}`;
-        } else if (
-          x0 === 0 &&
-          x1 === 0 &&
-          x2 === 0 &&
-          ((y0 >= y1 && y1 >= y2) || (y0 <= y1 && y1 <= y2))
-        ) {
+
+        const slope1 = (y1 - y0) / (x1 - x0);
+        const slope2 = (y2 - y0) / (x2 - x0);
+
+        if (slope1 === slope2) {
           return `l${x2} ${y2}`;
-        } else if (
-          y0 === 0 &&
-          y1 === 0 &&
-          y2 === 0 &&
-          ((x0 >= x1 && x1 >= x2) || (x0 <= x1 && x1 <= x2))
-        ) {
-          return `l${x2} ${y2}`;
+        } else if (x0 === 0 && y0 === 0 && x2 === 0 && y2 === 0) {
+          return "";
         } else {
           return match;
         }
