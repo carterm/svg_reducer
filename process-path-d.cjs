32ec6c43ebfcf58c6929934d43963cb468e0c098
Carter Medlin <carterm@users.noreply.github.com>
2026-06-11 11:44:19 -0700

    sequential q

--- process-path-d.cjs
+++ process-path-d.cjs
@@ -488,6 +488,9 @@ const processPathD = (pathD, options, pathElement) => {
     pathD = pathD.replace(/c([^lshvzqmaA-Z]*)/gms, match =>
       `c${match.replace(/c-/gms, "-")}`.replace(/cc/gms, "c")
     ); // Combine consecutive "c-" command codes
+    pathD = pathD.replace(/q([^cshvzlmaA-Z]*)/gms, match =>
+      `q${match.replace(/q-/gms, "-")}`.replace(/qq/gms, "q")
+    );
     pathD = pathD.replace(/l([^cshvzqmaA-Z]*)/gms, match =>
       `l${match.replace(/l-/gms, "-")}`.replace(/ll/gms, "l")
     ); // Combine consecutive "l-" command codes
